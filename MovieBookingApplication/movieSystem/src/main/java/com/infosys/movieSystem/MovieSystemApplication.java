ac01626596228b729ee93a33919c408ef7e25938
Aditya-Chopra30 <31057csiot@gmail.com>
2024-12-02 13:42:52 +0530

    Movie Booking Application

---
+++ MovieBookingApplication/movieSystem/src/main/java/com/infosys/movieSystem/MovieSystemApplication.java
@@ -0,0 +1,18 @@
+package com.infosys.movieSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieSystemApplication.class, args);
+	}
+
+	// UserName --> Lotus PassWord --> 1234 --> Admin
+	// UserName --> tulip PassWord --> apple -->customer
+	//UserName --> Lilly Password --> white --> Customer
+	
+	//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
+}
